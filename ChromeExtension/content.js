3a613696a8c24eb3d35ce1a1457cabae64b985f9
Russell Elliott <russell@quadsquad.net>
2023-02-05 14:30:27 -0800

    removed useless right table column

---
+++ ChromeExtension/content.js
@@ -0,0 +1,18 @@
+//https://stackoverflow.com/questions/5897122/accessing-elements-by-type-in-javascript
+//https://www.w3schools.com/jsref/met_element_getelementsbytagname.asp
+var tables = document.getElementsByTagName("table");
+
+//var tables = document.querySelectorAll('table');
+var mainTable = tables[0];
+console.log(mainTable.innerHTML);
+
+//https://www.geeksforgeeks.org/how-to-remove-column-from-html-table-using-javascript/
+// Getting the rows in table.
+var row = mainTable.rows; 
+// Removing the column at index(1).  
+var i = 4; 
+for (var j = 0; j < row.length; j++) {
+
+    // Deleting the ith cell of each row.
+    row[j].deleteCell(i);
+}
